e08f424aed97189a0b7b0beed6548762d7f9ed58
rda-wtag <rajib.das@welldev.io>
2020-10-10 22:04:19 +0600

    remove unused scss file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,6 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import WebpackerReact from 'webpacker-react';
 import 'bootstrap';
 import '../shared/i18n-js';
 
