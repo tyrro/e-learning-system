1cabced0515a9c9fe519f5ca1df0f137256d260f
rda-wtag <rajib.das@welldev.io>
2020-10-14 23:49:51 +0600

    update seeds and readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,224 @@
 Course.create(
   [
     {
-      name: 'Special title treatment',
-      description: 'With supporting text below as a natural lead-in.',
+      name: 'Welcome to Sample Course 1',
+      description: 'In Sample Course we will learn about the topic xyz',
       lessons_attributes: [
         {
           name: 'This Lesson is under course 1',
-          description: 'Hello World, do not blame the rape victims, blame the perpetrator',
+          description: 'This is a sample description',
+          questions_attributes: [
+            {
+              name: 'Did the Germans ever capture Moscow?',
+              answers_attributes: [
+                {
+                  name: 'yes',
+                  correct: false,
+                },
+                {
+                  name: 'no',
+                  correct: true,
+                },
+              ],
+            },
+            {
+              name: 'How did Hitler die?',
+              answers_attributes: [
+                {
+                  name: 'Hanged after the trials of Nuremberg',
+                  correct: false,
+                },
+                {
+                  name: 'Shot by the Russians                  ',
+                  correct: false,
+                },
+                {
+                  name: 'Suicide',
+                  correct: true,
+                },
+                {
+                  name: 'No one knows                  ',
+                  correct: false,
+                },
+              ],
+            },
+            {
+              name: 'Which of these countries did NOT fight in WWII?',
+              answers_attributes: [
+                {
+                  name: 'Finland',
+                  correct: false,
+                },
+                {
+                  name: 'Greece',
+                  correct: false,
+                },
+                {
+                  name: 'Ireland',
+                  correct: true,
+                },
+                {
+                  name: 'Italy',
+                  correct: false,
+                },
+              ],
+            },
+            {
+              name: 'What was the name of the American effort to build an atomic bomb?              ',
+              answers_attributes: [
+                {
+                  name: 'The Baltimore Project                  ',
+                  correct: false,
+                },
+                {
+                  name: 'The Chicago Project',
+                  correct: false,
+                },
+                {
+                  name: 'The Manhattan Project',
+                  correct: true,
+                },
+                {
+                  name: 'The Philadelphia Project',
+                  correct: false,
+                },
+              ],
+            },
+            {
+              name: 'About 70 million people died as a result of WWII. What percent of these people were from
+              the United States?',
+              answers_attributes: [
+                {
+                  name: '0.6%',
+                  correct: true,
+                },
+                {
+                  name: '5.6%',
+                  correct: false,
+                },
+                {
+                  name: '15.6%',
+                  correct: false,
+                },
+                {
+                  name: '330.6%',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: 'This Lesson is under course 1',
+          description: 'This is a sample description',
+          questions_attributes: [
+            {
+              name: 'Did the Germans ever capture Moscow?',
+              answers_attributes: [
+                {
+                  name: 'yes',
+                  correct: false,
+                },
+                {
+                  name: 'no',
+                  correct: true,
+                },
+              ],
+            },
+            {
+              name: 'How did Hitler die?',
+              answers_attributes: [
+                {
+                  name: 'Hanged after the trials of Nuremberg',
+                  correct: false,
+                },
+                {
+                  name: 'Shot by the Russians                  ',
+                  correct: false,
+                },
+                {
+                  name: 'Suicide',
+                  correct: true,
+                },
+                {
+                  name: 'No one knows                  ',
+                  correct: false,
+                },
+              ],
+            },
+            {
+              name: 'Which of these countries did NOT fight in WWII?',
+              answers_attributes: [
+                {
+                  name: 'Finland',
+                  correct: false,
+                },
+                {
+                  name: 'Greece',
+                  correct: false,
+                },
+                {
+                  name: 'Ireland',
+                  correct: true,
+                },
+                {
+                  name: 'Italy',
+                  correct: false,
+                },
+              ],
+            },
+            {
+              name: 'What was the name of the American effort to build an atomic bomb?              ',
+              answers_attributes: [
+                {
+                  name: 'The Baltimore Project                  ',
+                  correct: false,
+                },
+                {
+                  name: 'The Chicago Project',
+                  correct: false,
+                },
+                {
+                  name: 'The Manhattan Project',
+                  correct: true,
+                },
+                {
+                  name: 'The Philadelphia Project',
+                  correct: false,
+                },
+              ],
+            },
+            {
+              name: 'About 70 million people died as a result of WWII. What percent of these people were from
+              the United States?',
+              answers_attributes: [
+                {
+                  name: '0.6%',
+                  correct: true,
+                },
+                {
+                  name: '5.6%',
+                  correct: false,
+                },
+                {
+                  name: '15.6%',
+                  correct: false,
+                },
+                {
+                  name: '330.6%',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'Welcome to Sample Course 2',
+      description: 'In Sample Course we will learn about the topic xyz',
+      lessons_attributes: [
+        {
+          name: 'This Lesson is under course 2',
+          description: 'This is a sample description',
           questions_attributes: [
             {
               name: 'Did the Germans ever capture Moscow?',
@@ -109,316 +321,7 @@ Course.create(
         },
         {
           name: 'This Lesson is under course 2',
-          description: 'Hello World, do not blame the rape victims, blame the perpetrator',
-          questions_attributes: [
-            {
-              name: 'Did the Germans ever capture Moscow?',
-              answers_attributes: [
-                {
-                  name: 'yes',
-                  correct: false,
-                },
-                {
-                  name: 'no',
-                  correct: true,
-                },
-              ],
-            },
-            {
-              name: 'How did Hitler die?',
-              answers_attributes: [
-                {
-                  name: 'Hanged after the trials of Nuremberg',
-                  correct: false,
-                },
-                {
-                  name: 'Shot by the Russians                  ',
-                  correct: false,
-                },
-                {
-                  name: 'Suicide',
-                  correct: true,
-                },
-                {
-                  name: 'No one knows                  ',
-                  correct: false,
-                },
-              ],
-            },
-            {
-              name: 'Which of these countries did NOT fight in WWII?',
-              answers_attributes: [
-                {
-                  name: 'Finland',
-                  correct: false,
-                },
-                {
-                  name: 'Greece',
-                  correct: false,
-                },
-                {
-                  name: 'Ireland',
-                  correct: true,
-                },
-                {
-                  name: 'Italy',
-                  correct: false,
-                },
-              ],
-            },
-            {
-              name: 'What was the name of the American effort to build an atomic bomb?              ',
-              answers_attributes: [
-                {
-                  name: 'The Baltimore Project                  ',
-                  correct: false,
-                },
-                {
-                  name: 'The Chicago Project',
-                  correct: false,
-                },
-                {
-                  name: 'The Manhattan Project',
-                  correct: true,
-                },
-                {
-                  name: 'The Philadelphia Project',
-                  correct: false,
-                },
-              ],
-            },
-            {
-              name: 'About 70 million people died as a result of WWII. What percent of these people were from
-              the United States?',
-              answers_attributes: [
-                {
-                  name: '0.6%',
-                  correct: true,
-                },
-                {
-                  name: '5.6%',
-                  correct: false,
-                },
-                {
-                  name: '15.6%',
-                  correct: false,
-                },
-                {
-                  name: '330.6%',
-                },
-              ],
-            },
-          ],
-        },
-        {
-          name: 'This Lesson is under course 3',
-          description: 'Hello World, do not blame the rape victims, blame the perpetrator',
-          questions_attributes: [
-            {
-              name: 'Did the Germans ever capture Moscow?',
-              answers_attributes: [
-                {
-                  name: 'yes',
-                  correct: false,
-                },
-                {
-                  name: 'no',
-                  correct: true,
-                },
-              ],
-            },
-            {
-              name: 'How did Hitler die?',
-              answers_attributes: [
-                {
-                  name: 'Hanged after the trials of Nuremberg',
-                  correct: false,
-                },
-                {
-                  name: 'Shot by the Russians                  ',
-                  correct: false,
-                },
-                {
-                  name: 'Suicide',
-                  correct: true,
-                },
-                {
-                  name: 'No one knows                  ',
-                  correct: false,
-                },
-              ],
-            },
-            {
-              name: 'Which of these countries did NOT fight in WWII?',
-              answers_attributes: [
-                {
-                  name: 'Finland',
-                  correct: false,
-                },
-                {
-                  name: 'Greece',
-                  correct: false,
-                },
-                {
-                  name: 'Ireland',
-                  correct: true,
-                },
-                {
-                  name: 'Italy',
-                  correct: false,
-                },
-              ],
-            },
-            {
-              name: 'What was the name of the American effort to build an atomic bomb?              ',
-              answers_attributes: [
-                {
-                  name: 'The Baltimore Project                  ',
-                  correct: false,
-                },
-                {
-                  name: 'The Chicago Project',
-                  correct: false,
-                },
-                {
-                  name: 'The Manhattan Project',
-                  correct: true,
-                },
-                {
-                  name: 'The Philadelphia Project',
-                  correct: false,
-                },
-              ],
-            },
-            {
-              name: 'About 70 million people died as a result of WWII. What percent of these people were from
-              the United States?',
-              answers_attributes: [
-                {
-                  name: '0.6%',
-                  correct: true,
-                },
-                {
-                  name: '5.6%',
-                  correct: false,
-                },
-                {
-                  name: '15.6%',
-                  correct: false,
-                },
-                {
-                  name: '330.6%',
-                },
-              ],
-            },
-          ],
-        },
-        {
-          name: 'This Lesson is under course 4',
-          description: 'Hello World, do not blame the rape victims, blame the perpetrator',
-          questions_attributes: [
-            {
-              name: 'Did the Germans ever capture Moscow?',
-              answers_attributes: [
-                {
-                  name: 'yes',
-                  correct: false,
-                },
-                {
-                  name: 'no',
-                  correct: true,
-                },
-              ],
-            },
-            {
-              name: 'How did Hitler die?',
-              answers_attributes: [
-                {
-                  name: 'Hanged after the trials of Nuremberg',
-                  correct: false,
-                },
-                {
-                  name: 'Shot by the Russians                  ',
-                  correct: false,
-                },
-                {
-                  name: 'Suicide',
-                  correct: true,
-                },
-                {
-                  name: 'No one knows                  ',
-                  correct: false,
-                },
-              ],
-            },
-            {
-              name: 'Which of these countries did NOT fight in WWII?',
-              answers_attributes: [
-                {
-                  name: 'Finland',
-                  correct: false,
-                },
-                {
-                  name: 'Greece',
-                  correct: false,
-                },
-                {
-                  name: 'Ireland',
-                  correct: true,
-                },
-                {
-                  name: 'Italy',
-                  correct: false,
-                },
-              ],
-            },
-            {
-              name: 'What was the name of the American effort to build an atomic bomb?              ',
-              answers_attributes: [
-                {
-                  name: 'The Baltimore Project                  ',
-                  correct: false,
-                },
-                {
-                  name: 'The Chicago Project',
-                  correct: false,
-                },
-                {
-                  name: 'The Manhattan Project',
-                  correct: true,
-                },
-                {
-                  name: 'The Philadelphia Project',
-                  correct: false,
-                },
-              ],
-            },
-            {
-              name: 'About 70 million people died as a result of WWII. What percent of these people were from
-              the United States?',
-              answers_attributes: [
-                {
-                  name: '0.6%',
-                  correct: true,
-                },
-                {
-                  name: '5.6%',
-                  correct: false,
-                },
-                {
-                  name: '15.6%',
-                  correct: false,
-                },
-                {
-                  name: '330.6%',
-                },
-              ],
-            },
-          ],
-        },
-        {
-          name: 'This Lesson is under course 5',
-          description: 'Hello World, do not blame the rape victims, blame the perpetrator',
+          description: 'This is a sample description',
           questions_attributes: [
             {
               name: 'Did the Germans ever capture Moscow?',
