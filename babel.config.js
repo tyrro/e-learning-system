762d16926045d8252c0f83080efa841ea0593f58
rajibds <rajib.das.shuva@gmail.com>
2022-01-09 12:24:25 +0600

    chore: update rails/webpacker to v5

--- babel.config.js
+++ babel.config.js
@@ -64,6 +64,18 @@ module.exports = function (api) {
         },
       ],
       [
+        '@babel/plugin-proposal-private-methods',
+        {
+          loose: true,
+        },
+      ],
+      [
+        '@babel/plugin-proposal-private-property-in-object',
+        {
+          loose: true,
+        },
+      ],
+      [
         '@babel/plugin-transform-runtime',
         {
           helpers: false,
